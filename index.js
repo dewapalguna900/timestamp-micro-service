e6fbb5927f9df4e7ec86373117b091837b973e56
Dewapalguna <dewapalguna900@gmail.com>
2023-12-11 20:29:49 +0800

    Push local project to github

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/api/', (req, res) => {
+    const date = new Date();
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+
+    res.json({
+        unix: unix,
+        utc: utc
+    });
+})
+
+app.get('/api/:date', (req, res) => {
+    const date = new Date(req.params.date);
+    let unix = '';
+    let utc = '';
+    let error = false;
+    if(!isNaN(date)){
+        unix = date.getTime();
+        utc = date.toUTCString();
+    }else{
+        let unixTimestamp = parseInt(req.params.date);
+        let dateConverted = new Date(unixTimestamp);
+        
+        if(isNaN(dateConverted)){
+            error = true;
+        } else{
+            unix = unixTimestamp;
+            utc = dateConverted.toUTCString();
+        }
+    }
+
+    if(!error){
+        res.json({
+            unix: unix,
+            utc: utc
+        });
+    } else{
+        res.json({
+            error: "Invalid Date"
+        });
+    }
+})
